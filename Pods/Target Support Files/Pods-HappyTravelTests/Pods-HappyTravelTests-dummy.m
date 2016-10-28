b5a64ad07a64ccef3461d4abd4442a573de2c8e7
陈奕涛 <chenyitao@chenyitaodeMacbook-Pro.local>
2016-10-28 14:38:39 +0800

    rm Pods Podfile.lock

--- Pods/Target Support Files/Pods-HappyTravelTests/Pods-HappyTravelTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HappyTravelTests : NSObject
-@end
-@implementation PodsDummy_Pods_HappyTravelTests
-@end
--- Pods/Target Support Files/Pods-HappyTravelTests/Pods-HappyTravelTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_HappyTravelTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_HappyTravelTestsVersionString[];
-
