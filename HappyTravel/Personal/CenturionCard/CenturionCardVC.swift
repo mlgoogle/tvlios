20affa2cfa078c517f15f93088a026d0936826a6
BBRick <jiaobiao0505@163.com>
2016-12-22 14:20:49 +0800

    fixedbugs

--- HappyTravel/Personal/CenturionCard/CenturionCardVC.swift
+++ HappyTravel/Personal/CenturionCard/CenturionCardVC.swift
@@ -88,6 +88,7 @@ class CenturionCardVC: UIViewController, UITableViewDelegate, UITableViewDataSou
             DataManager.updateUserInfo(userInfo)
             let chatVC = ChatVC()
             chatVC.servantInfo = userInfo
+            chatVC.isServersMan = true
             navigationController?.pushViewController(chatVC, animated: true)
             
         }
